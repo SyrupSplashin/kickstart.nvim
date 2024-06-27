9204ca2825eb18b3d40ecf1f5ebb243b45cb5822
SyrupSplashin <vertex@syrupsplash.in>
2024-06-26 21:24:29 -0400

    Changed a few thangs, removed a thang or two

--- lua/custom/binds/binds.lua
+++ lua/custom/binds/binds.lua
@@ -14,17 +14,11 @@ vim.keymap.set('n', '<A-h>', '[[<cmd>vertical resize -5<cr>]]', { desc = 'Reduce
 vim.keymap.set('n', '<A-l>', '[[<cmd>vertical resize +5<cr>]]', { desc = 'Increase horizontal size' })
 vim.keymap.set('n', '<A-j>', '[[<cmd>horizontal resize -5<cr>]]', { desc = 'Reduce vertical size' })
 vim.keymap.set('n', '<A-k>', '[[<cmd>horizontal resize +5<cr>]]', { desc = 'Increase vertical size' })
--- Format Command
-vim.api.nvim_create_user_command("Format", function(args)
-	local range = nil
-	if args.count ~= -1 then
-		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-		range = {
-			start = { args.line1, 0 },
-			["end"] = { args.line2, end_line:len() },
-		}
-	end
-	require("conform").format({ async = true, lsp_fallback = true, range = range })
-end, { range = true, desc = 'Format current buffer with Conform' })
 
-vim.keymap.set('n', '<leader>xf', ':Format<CR>', { desc = 'Format Code'})
+vim.keymap.set('n', '<leader>/', function()
+	-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+	builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 0,
+		previewer = false,
+	})
+end, { desc = '[/] Fuzzily search in current buffer' })
